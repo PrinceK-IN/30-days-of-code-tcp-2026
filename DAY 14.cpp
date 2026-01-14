eef0f37ceed62e8b2d54909aee6ce2adbac6d9b5
PrinceK-IN <prince.princekumar.in@gmail.com>
2026-01-14 22:00:05 +0530

    Create DAY 14.cpp

---
+++ DAY 14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int count = 0;
+        while (n > 0) {
+        if ((n & 1) == 1) {
+            count++;
+        }
+        n = n >> 1;
+    }
+    return count;
+    }
+};
