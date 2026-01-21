ddfa6d0cda6aaf515fd0449733a577eefffa8f39
PrinceK-IN <prince.princekumar.in@gmail.com>
2026-01-21 09:17:01 +0530

    Create DAY 21.cpp

---
+++ DAY 21.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nextGreaterMap;
+        stack<int> s;
+
+        for (int num : nums2) {
+            while (!s.empty() && num > s.top()) {
+                nextGreaterMap[s.top()] = num;
+                s.pop();
+            }
+            s.push(num);
+        }
+        
+        vector<int> result;
+        for (int num : nums1) {
+            if (nextGreaterMap.find(num) != nextGreaterMap.end()) {
+                result.push_back(nextGreaterMap[num]);
+            } else {
+                result.push_back(-1);
+            }
+        }
+        return result;
+    }
+};
